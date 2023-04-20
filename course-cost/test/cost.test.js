7d8ad1730070006556589a2f52c1aba36158b4e4
Londeka Zikalala <londekazikalala@gmail.com>
2023-04-20 16:27:32 +0200

    css

--- course-cost/test/cost.test.js
+++ course-cost/test/cost.test.js
@@ -2,29 +2,49 @@ describe("The courseCost function", function () {
 
     it("should return the course cost without a discount", function () {
 
-        const cost = courseCost("r1", "2023-05-21")
+        const cost1 = courseCost("r1", "2023-05-21")
 
-        assert.deepEqual(cost, {
-            "status" : "success",
-            "code": "r1",
-            "description" : "",
-            "cost" : "",
-            "discount" : "", 
-            "amountDue" : ""
+        assert.deepEqual(cost1, {
+            'status' : 'success',
+            'code': 'r1',
+            'description' : 'Ruby on Rails intro',
+            'cost' : 'R5675.00',
+            'discount': 'R0.00', 
+            'amountDue' : 'R5675.00'
         });
 
+        const cost2 = courseCost("p1", "2023-05-21")
+
+        assert.deepEqual(cost2, {
+            'status' : 'success',
+            'code': 'p1',
+            'description' : 'Python',
+            'cost' : 'R7850.00',
+            'discount': 'R0.00', 
+            'amountDue' : 'R7850.00'
+        });
+        const cost3 = courseCost("p2", "2023-05-21")
+
+        assert.deepEqual(cost3, {
+            'status' : 'success',
+            'code': 'p2',
+            'description' : 'Intro to PHP course',
+            'cost' : 'R8990.00',
+            'discount': 'R0.00', 
+            'amountDue' : 'R8990.00'
+        });
 
     })
     it("should return the course cost with  a 20% discount", function () {
         const cost = courseCost("r1", "2023-05-21")
 
         assert.deepEqual(cost, {
-            "status" : "success",
-            "code": "r1",
-            "description" : "",
-            "cost" : "",
-            "discount" : "", 
-            "amountDue" : ""
+            'status' : 'success',
+            'code': 'p2',
+            'description' : 'Intro to PHP course',
+            'cost' : 'R1798.00',
+            'discount' : '', 
+            'amountDue' : 'R7192.00'
         });
 
     })
